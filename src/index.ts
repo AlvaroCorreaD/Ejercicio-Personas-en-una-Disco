8985b4b0d4840af42e0f8ea3f6986e81550c0cfc
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-29 18:39:30 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let arreglo1: number[] = new Array(270)
+let indice: number;
+
+function random(min = 0, max = 270) {
+}
+
+if (indice < 21) {
+  console.log("La cantidad de personas menores de 21 años es:", arreglo1[indice]);
+
+} else if (indice >== 21) {
+  console.log("La cantidad de personas de 21 años en adelante es:", arreglo1[indice]);
+
+} else {
+  console.log("La cantidad total de personas en el local es:", arreglo1[indice]);
+}
