61da8cbe2d19c8f76dfb6544f596b697e2d5409d
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-30 20:05:43 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-let arreglo1: number[] = new Array(270)
+// let arreglo: number[] = new Array(270);
+// let indice: number;
+// let n: number;
+
+// function arrayAleatorio(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// for (indice = 0; indice < n; indice++) {
+//   n = arrayAleatorio(18,41)
+//   console.log(
+//     "La cantidad de personas menores de 21 años es:",
+//     arrayAleatorio(18, 21)
+//   );
+let arreglo: number[] = new Array(270);
 let indice: number;
 
-function random(min = 0, max = 270) {
+function arrayAleatorio() {
+  return Math.floor(Math.random() * (41 - 18)) + 18;
+}
+
+for (indice = 0; indice < 270; indice++) {
+  arreglo[indice] = arrayAleatorio(18, 40);
+  console.log(arreglo[indice]);
 }
 
 if (indice < 21) {
-  console.log("La cantidad de personas menores de 21 años es:", arreglo1[indice]);
-
-} else if (indice >== 21) {
-  console.log("La cantidad de personas de 21 años en adelante es:", arreglo1[indice]);
-
-} else {
-  console.log("La cantidad total de personas en el local es:", arreglo1[indice]);
+  console.log("La cantidad de personas menores a 21 años es:");
 }
